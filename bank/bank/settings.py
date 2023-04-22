b3be84aabd7ac25212ecc30fb2279e7b3448a768
0sigma101 <pallav.gupta2021@vitstudent.ac.in>
2023-04-22 16:21:58 +0530

    Fixing Security key haha

--- bank/bank/settings.py
+++ bank/bank/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n!_8ta496tr*-imxk-m_%0(hds%sqq(70tezjfy6asv^!q+_q3'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
