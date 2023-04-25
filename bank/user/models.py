b17c61f17e22afe505e85cf823cfdf549fa72a30
0sigma101 <pallav.gupta2021@vitstudent.ac.in>
2023-04-25 21:51:33 +0530

    This is the user account

--- bank/user/models.py
+++ bank/user/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from datetime import datetime
+from datetime import datetime,date
 # Create your models here.
 class Customer (models.Model):
     fname = models.CharField(max_length=30)
     lname = models.CharField(max_length=30)
     city = models.CharField(max_length=15)
     mobileno = models.CharField(max_length=10)
-    dob = models.DateTimeField("date of birth",default=datetime.today())
+    dob = models.DateTimeField("date of birth")
     
     def __int__(self) -> int:
         return self.id      
@@ -14,12 +14,15 @@ class Customer (models.Model):
 
 class Account (models.Model):
     cust_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    aod = models.DateField('last accessed')
+    aod = models.DateField('last accessed',default=date.today())
     savings = models.IntegerField(default=0)
     current = models.IntegerField(default=0)
     fixed = models.IntegerField(default=0)
     pin = models.IntegerField()
     loan = models.IntegerField(default=0)
-    loan_date = models.DateField('loan date')
-    fixed_date = models.DateField('fixed date')
+    loan_date = models.DateField('loan date',null=True)
+    fixed_date = models.DateField('fixed date',null=True)
     principle_amt = models.IntegerField(default = 0)
+    
+    def __int__(self) -> int:
+        return self.id
