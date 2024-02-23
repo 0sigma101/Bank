a313e60d5e57ac6d943abbf0d0ce1bb5d274cdd8
0sigma101 <pallav.gupta2021@vitstudent.ac.in>
2024-02-23 13:40:16 +0530

    Added few views

--- khush_raho_bank/members/views.py
+++ khush_raho_bank/members/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import *
 
 def members(request):
-    template = loader.get_template('login.html')
-    return HttpResponse(template.render())
+    mymembers = Account.objects.all().values()
+    template = loader.get_template('datashowfilter.html')
+    context = {
+    'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context,request))
+
+def details(request,id):
+    mymember = Customer.objects.get(custid=id)
+    template = loader.get_template('details.html')
+    context = {
+    'mymember': mymember,
+    }
+    return HttpResponse(template.render(context,request))
