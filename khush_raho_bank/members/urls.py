4d42db7627e0d5d685027ac5df3211a713ee9292
0sigma101 <pallav.gupta2021@vitstudent.ac.in>
2024-03-22 21:04:17 +0530

    Added login and signup page

--- khush_raho_bank/members/urls.py
+++ khush_raho_bank/members/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members, name='members'),
+    path('members/', views.loginsignup, name='loginsignup'),
     path('members/details/<int:id>', views.details, name='details'),
+    path('members/loginprocess', views.loginprocess, name='loginprocess'),
+    path('members/signupprocess', views.signupprocess, name='signupprocess'),
 ]
--- khush_raho_bank/members/views.py
+++ khush_raho_bank/members/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import *
+from datetime import datetime
+
+def custid():
+    custid = Customer.objects.all().values("custid")[len(Customer.objects.all().values("custid"))-1]['custid']+1
+    return custid
+
+def acnum():
+    acnum = Account.objects.all().values("acnumber")[len(Account.objects.all().values("acnumber"))-1]['acnumber']+1
+    return acnum
+
+def redeem(code):
+    person = Account.objects.get(acnumber=code)
+    person.current_account+=200
+    person.save()
 
 def members(request):
     mymembers = Account.objects.all().values()
@@ -10,6 +24,52 @@ def members(request):
     'mymembers': mymembers,
     }
     return HttpResponse(template.render(context,request))
+
+def loginsignup(request):
+    template = loader.get_template('loginsignup.html')
+    context = {}
+    return HttpResponse(template.render(context,request))
+    
+def loginprocess(request):
+    if request.method == "POST":
+        acnumber = request.POST['acnumber']
+        password = request.POST['passwd']
+        try:
+            user = Account.objects.get(acnumber=acnumber, pin=password)
+            # You can also use Django's built-in authentication
+            # from django.contrib.auth import authenticate, login
+            # user = authenticate(request, acnumber=acnumber, password=password)
+            if user:
+                # Login successful, redirect to a success page or homepage
+                return HttpResponse("Login Success")
+            else:
+                # Login failed
+                return HttpResponse("Login Failed")
+        except AuthUser.DoesNotExist:
+            # Login failed
+            return HttpResponse("Login Failed")
+    template = loader.get_template('login.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def signupprocess(request):
+    if request.method == "POST":
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        city = request.POST['city']
+        mobileno = request.POST['mobileno']
+        email = request.POST['email']
+        dob = request.POST['dob']
+        pin = request.POST['pin']
+        code = request.POST['code']
+        redeem(code)
+        Account_data = Account(acnumber = acnum(),custid = custid(),aod = datetime.today(),savings_account = 0,current_account = 0,fixed_deposit = 0,pin = pin)
+        Customer_data = Customer(custid = custid(),fname = fname,lname = lname,city = city,mobileno = mobileno,email = email,dob = dob,code = Account_data.acnumber)
+        Account_data.save()
+        Customer_data.save()
+    template = loader.get_template('signup.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
 
 def details(request,id):
     mymember = Customer.objects.get(custid=id)
