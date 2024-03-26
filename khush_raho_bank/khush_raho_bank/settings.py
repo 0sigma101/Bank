fcab85510c6909499e28f9a0cd18364eae4be8e5
Pallav Gupta <95297658+0sigma101@users.noreply.github.com>
2024-03-26 23:47:56 +0530

    Update settings.py

--- khush_raho_bank/khush_raho_bank/settings.py
+++ khush_raho_bank/khush_raho_bank/settings.py
@@ -19,12 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%-d@lce81l8m_7&$wl=($)xh(i66depjmp08+0-7(ho24&5yc0'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
